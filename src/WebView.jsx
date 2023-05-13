dff9ba4b9fc07c49e03b16ac340567245204b6cd
adislksn <adi.120140038@student.itera.ac.id>
2023-05-13 15:37:49 +0700

    Update BackHandler

--- src/WebView.jsx
+++ src/WebView.jsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { WebView } from 'react-native-webview';
-import { useState, useCallback, useRef } from 'react';
+import { useState, useCallback, useRef, useEffect } from 'react';
 import {
-    RefreshControl, SafeAreaView, ScrollView, StyleSheet, View,
+    RefreshControl, SafeAreaView, ScrollView, StyleSheet, View, BackHandler, ToastAndroid
 } from 'react-native';
 import appConfig from '../app.conf.js';
 
 const WebViewMain = () => {
     const webViewRef = useRef();
-
+    const [canGoBack, setCanGoBack] = useState(false);
     const [enabledRefreshing, setEnabledRefreshing] = useState(false);
     const [refreshing, setRefreshing] = useState(false);
 
@@ -22,6 +22,44 @@ const WebViewMain = () => {
         webViewRef.current.reload();
         setTimeout(() => setRefreshing(false), 200);
     });
+
+    const handleBackButton = useCallback(() => {
+        if (canGoBack) {
+            webViewRef.current?.goBack();
+            return true;
+        } else {
+            // ToastAndroid.show('Tekan sekali lagi untuk keluar', ToastAndroid.SHORT);
+            BackHandler.exitApp();
+            return true;
+        }
+    }, [canGoBack]);
+    
+    useEffect(() => {
+        BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+        return () => {
+            BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+        };
+    }, [handleBackButton]);
+    
+    const handleWebViewNavigationStateChange = (newState) => {
+        setCanGoBack(newState.canGoBack);
+    };
+    
+    // useEffect(() => {
+    //     const handleBackButton = () => {
+    //         // Periksa apakah halaman webview memiliki history
+    //         webViewRef.current?.goBack();
+    //         return true;
+    //     };
+    
+    //     // Daftarkan event listener untuk tombol "Back"
+    //     BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+    
+    //     return () => {
+    //         // Hapus event listener saat komponen unmount
+    //         BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+    //     };
+    // }, []);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -47,6 +85,7 @@ const WebViewMain = () => {
                         domStorageEnabled={true}
                         startInLoadingState={true}
                         onMessage={(event) => watchWebViewMessage(JSON.parse(event.nativeEvent.data))}
+                        onNavigationStateChange={handleWebViewNavigationStateChange}
                     />
 
                 </View>

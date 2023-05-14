0fceec67b13d9d16de77018510e7f70537d0905c
adislksn <adi.120140038@student.itera.ac.id>
2023-05-15 04:18:52 +0700

    Update whatsappMobile handler

--- src/WebView.jsx
+++ src/WebView.jsx
@@ -1,9 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { WebView } from 'react-native-webview';
 import { useState, useCallback, useRef, useEffect } from 'react';
-import {
-    RefreshControl, SafeAreaView, ScrollView, StyleSheet, View, BackHandler, ToastAndroid
-} from 'react-native';
+import { RefreshControl, SafeAreaView, ScrollView, StyleSheet, View, BackHandler, ToastAndroid, Linking } from 'react-native';
 import appConfig from '../app.conf.js';
 
 const WebViewMain = () => {
@@ -17,79 +15,72 @@ const WebViewMain = () => {
         else setEnabledRefreshing(false);
     };
 
-    const onWebRefresh = () => useCallback(() => {
+    const onWebRefresh = useCallback(() => {
         setRefreshing(true);
         webViewRef.current.reload();
         setTimeout(() => setRefreshing(false), 200);
-    });
+    }, []);
 
     const handleBackButton = useCallback(() => {
         if (canGoBack) {
-            webViewRef.current?.goBack();
-            return true;
+        webViewRef.current?.goBack();
+        return true;
         } else {
-            // ToastAndroid.show('Tekan sekali lagi untuk keluar', ToastAndroid.SHORT);
-            BackHandler.exitApp();
-            return true;
+        // ToastAndroid.show('Tekan sekali lagi untuk keluar', ToastAndroid.SHORT);
+        BackHandler.exitApp();
+        return true;
         }
     }, [canGoBack]);
-    
+
     useEffect(() => {
         BackHandler.addEventListener('hardwareBackPress', handleBackButton);
         return () => {
-            BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+        BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
         };
     }, [handleBackButton]);
-    
+
     const handleWebViewNavigationStateChange = (newState) => {
+        const whatsappURL = 'https://wa.me/';
+        const whatsappMobile = 'whatsapp://';
+
+        if (newState.url.startsWith(whatsappURL)) {
+            // Redirect to the WhatsApp URL
+            Linking.openURL(newState.url);
+            return;
+        } else if (newState.url.startsWith(whatsappMobile)) {
+            // Redirect to the WhatsApp Mobile URL
+            Linking.openURL(newState.url);
+            return;
+        }
         setCanGoBack(newState.canGoBack);
     };
-    
-    // useEffect(() => {
-    //     const handleBackButton = () => {
-    //         // Periksa apakah halaman webview memiliki history
-    //         webViewRef.current?.goBack();
-    //         return true;
-    //     };
-    
-    //     // Daftarkan event listener untuk tombol "Back"
-    //     BackHandler.addEventListener('hardwareBackPress', handleBackButton);
-    
-    //     return () => {
-    //         // Hapus event listener saat komponen unmount
-    //         BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
-    //     };
-    // }, []);
 
     return (
         <SafeAreaView style={styles.container}>
-            <StatusBar style="dark" />
-            <ScrollView
-                contentContainerStyle={styles.scrollView}
-                refreshControl={
-                    <RefreshControl enabled={enabledRefreshing} refreshing={refreshing} onRefresh={onWebRefresh()} />
-                }>
-                <View
-                    style={{ flex: 1, width: '100%' }}
-                    onStartShouldSetResponder={(e) => {
-                        const { pageY } = e.nativeEvent;
-                        if (pageY < 140) setEnabledRefreshing(true);
-                        else setEnabledRefreshing(false);
-                    }}>
-
-                    <WebView
-                        ref = {webViewRef}
-                        source={{ uri: appConfig.webView.uri }}
-                        AllowsFullscreenVideo={true}
-                        javaScriptEnabled={true}
-                        domStorageEnabled={true}
-                        startInLoadingState={true}
-                        onMessage={(event) => watchWebViewMessage(JSON.parse(event.nativeEvent.data))}
-                        onNavigationStateChange={handleWebViewNavigationStateChange}
-                    />
-
-                </View>
-            </ScrollView>
+        <StatusBar style="dark" />
+        <ScrollView
+            contentContainerStyle={styles.scrollView}
+            refreshControl={
+            <RefreshControl enabled={enabledRefreshing} refreshing={refreshing} onRefresh={onWebRefresh} />
+            }
+        >
+            <View style={{ flex: 1, width: '100%' }} onStartShouldSetResponder={(e) => {
+            const { pageY } = e.nativeEvent;
+            if (pageY < 140) setEnabledRefreshing(true);
+            else setEnabledRefreshing(false);
+            }}>
+            <WebView
+                ref={webViewRef}
+                source={{ uri: appConfig.webView.uri }}
+                AllowsFullscreenVideo={true}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+                startInLoadingState={true}
+                onMessage={(event) => watchWebViewMessage(JSON.parse(event.nativeEvent.data))}
+                onNavigationStateChange={handleWebViewNavigationStateChange}
+            />
+            </View>
+        </ScrollView>
         </SafeAreaView>
     );
 };
@@ -97,7 +88,7 @@ const WebViewMain = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 30,
+        paddingTop: 40,
         backgroundColor: '#FFFFF',
     },
     scrollView: {
